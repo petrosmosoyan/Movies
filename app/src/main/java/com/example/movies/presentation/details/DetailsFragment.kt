773cfb28042e5203d1d10eb1c620da08f7ca868e
Petros Mosoyan <mosoyan.petros@gmail.com>
2026-03-28 15:24:03 +0400

    Initial Commit

--- app/src/main/java/com/example/movies/presentation/details/DetailsFragment.kt
+++ app/src/main/java/com/example/movies/presentation/details/DetailsFragment.kt
@@ -2,15 +2,22 @@ package com.example.movies.presentation.details
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.movies.R
 import com.example.movies.databinding.FragmentDetailsBinding
+import com.example.movies.domain.model.Details
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
@@ -22,12 +29,45 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailsBinding.bind(view)
 
+        binding?.run {
+            ViewCompat.setOnApplyWindowInsetsListener(toolbar) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+                val params = v.layoutParams as ViewGroup.MarginLayoutParams
+                params.topMargin = systemBars.top
+                root.layoutParams = params
+                insets
+            }
+
+            toolbar.setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.detailsUIState.collect { uiState ->
-
+                    updateUI(uiState.details ?: return@collect)
                 }
             }
+        }
+    }
+
+    private fun updateUI(details: Details) {
+        binding?.run {
+            val ratingValue = details.voteAverage ?: 0.0
+            val rating = "★ ${String.format(Locale.US, "%.1f", ratingValue)} / 10"
+            val budget = "$${details.budget ?: ""}"
+            val revenue = "$${details.revenue ?: ""}"
+
+            Glide.with(requireContext()).load(details.backdropPath).into(backdropImage)
+            Glide.with(requireContext()).load(details.posterPath).into(posterImage)
+            movieTitle.text = details.title
+            releaseDate.text = details.releaseDate
+            ratingText.text = rating
+            overviewText.text = details.overview
+            budgetText.text = budget
+            revenueText.text = revenue
         }
     }
 
